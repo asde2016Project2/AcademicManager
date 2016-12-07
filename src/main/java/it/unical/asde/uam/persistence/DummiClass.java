1f54c8cb3b95058f3c1ce70d8d2c98b9ab284ac8
kzhack <brunofr86@gmail.com>
2016-12-07 23:17:10 +0100

    Test commit doing #5

---
+++ src/main/java/it/unical/asde/uam/persistence/DummiClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unical.asde.uam.persistence;
+
+/**
+ *
+ * @author kz
+ */
+public class DummiClass {
+    
+    // to be delete, only for git test purpose
+    
+    
+}
