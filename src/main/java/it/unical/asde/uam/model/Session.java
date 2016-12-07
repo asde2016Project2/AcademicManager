b1936c0d4d1d60300eb59b888721cf6bfe59504f
paolaeli <paolaeli@paolaeli>
2016-12-07 16:04:47 +0100

    Added Session class

---
+++ src/main/java/it/unical/asde/uam/model/Session.java
@@ -0,0 +1,77 @@
+package it.unical.asde.uam.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "session")
+public class Session implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name = "session_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int sessionId;
+	
+	@Column(name = "startingDate")
+	private Date startingDate;
+
+	@Column(name = "endingDate")
+	private Date endingDate;
+	
+	@Column(name = "academicYear")
+	private int academicYear;
+	
+	public Session() {
+		this.sessionId = 0;
+		this.startingDate = new Date();
+		this.endingDate = new Date();
+		this.academicYear = 0;
+	}
+	
+	
+	public Session(int sessionId, Date startingDate, Date endingDate, int academicYear) {
+		super();
+		this.sessionId = sessionId;
+		this.startingDate = startingDate;
+		this.endingDate = endingDate;
+		this.academicYear = academicYear;
+	}
+	
+	public int getId() {
+		return sessionId;
+	}
+	public void setId(int id) {
+		this.sessionId = id;
+	}
+	public Date getStartingDate() {
+		return startingDate;
+	}
+	public void setStartingDate(Date startingDate) {
+		this.startingDate = startingDate;
+	}
+	public Date getEndingDate() {
+		return endingDate;
+	}
+	public void setEndingDate(Date endingDate) {
+		this.endingDate = endingDate;
+	}
+	public int getAcademicYear() {
+		return academicYear;
+	}
+	public void setAcademicYear(int academicYear) {
+		this.academicYear = academicYear;
+	}
+	
+}
