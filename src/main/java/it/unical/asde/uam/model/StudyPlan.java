b58ade54e7fa479eb9d8a7bb51c210cc4357272f
Fabrizio <Fabrizio@DESKTOP-F0IG54D.homenet.telecomitalia.it>
2016-12-08 17:39:05 +0100

    added DegreeCourse and StudyPlan models. Question DAO has not been solved.

---
+++ src/main/java/it/unical/asde/uam/model/StudyPlan.java
@@ -0,0 +1,62 @@
+package it.unical.asde.uam.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+public class StudyPlan implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column
+	private int studyPlanId;
+	
+	@Column(name="name",unique=true,nullable=false,length=256)
+	private String name;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="degree_course_id", nullable=false)
+	private DegreeCourse degreeCourse;
+	
+	public StudyPlan() {}
+
+	public StudyPlan(String name, DegreeCourse degreeCourse) {
+		super();
+		this.name = name;
+		this.degreeCourse = degreeCourse;
+	}
+
+	public int getStudyPlanId() {
+		return studyPlanId;
+	}
+
+	public void setStudyPlanId(int studyPlanId) {
+		this.studyPlanId = studyPlanId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public DegreeCourse getDegreeCourse() {
+		return degreeCourse;
+	}
+
+	public void setDegreeCourse(DegreeCourse degreeCourse) {
+		this.degreeCourse = degreeCourse;
+	}
+}
