d9a8f793543000cb91292d1dc8827b9a8fddca1d
kzhack <brunofr86@gmail.com>
2017-01-16 19:53:32 +0100

    fixed conflicts

---
+++ src/main/java/it/unical/asde/uam/helper/UserProfileHelper.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unical.asde.uam.helper;
+
+/**
+ *
+ * @author Francesco Bruno
+ */
+public class UserProfileHelper {
+ 
+    
+    public static final String _ADMINISTRATOR = "administrator";
+    public static final String _PROFESSOR = "professor";
+    public static final String _STUDENT = "student";
+    public static final String _GUEST = "guest";
+    
+}
